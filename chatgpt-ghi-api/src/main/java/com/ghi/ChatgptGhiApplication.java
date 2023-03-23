8844def7c149ffaf96e48ed06ef1150a79819e89
LGX_TvT <1336037686@qq.com>
2023-03-23 12:27:28 +0800

    初始化

---
+++ chatgpt-ghi-api/src/main/java/com/ghi/ChatgptGhiApplication.java
@@ -0,0 +1,13 @@
+package com.ghi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatgptGhiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatgptGhiApplication.class, args);
+    }
+
+}
---
+++ chatgpt-ghi-api/src/test/java/com/ghi/chatgptghi/ChatgptGhiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghi.chatgptghi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatgptGhiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
