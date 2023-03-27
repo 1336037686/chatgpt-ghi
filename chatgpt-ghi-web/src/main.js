3ded3840044c94660bc703d1487e36af82e603eb
1336037686 <1336037686@qq.com>
2023-03-27 23:49:00 +0800

    添加路由与代理

--- chatgpt-ghi-web/src/main.js
+++ chatgpt-ghi-web/src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import Antd from 'ant-design-vue';
 import App from './App.vue'
 import 'ant-design-vue/dist/antd.css'; // or 'ant-design-vue/dist/antd.less'
+import router from './router'
 
 Vue.config.productionTip = false;
 Vue.use(Antd);
 
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
