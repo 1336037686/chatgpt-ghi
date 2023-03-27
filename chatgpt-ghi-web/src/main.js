ffb164ed83edbe6ec9150fbad3bcf9f503265a13
LGX_TvT <1336037686@qq.com>
2023-03-27 19:03:03 +0800

    添加axios和antd-design ui依赖

--- chatgpt-ghi-web/src/main.js
+++ chatgpt-ghi-web/src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
+import Antd from 'ant-design-vue';
 import App from './App.vue'
+import 'ant-design-vue/dist/antd.css'; // or 'ant-design-vue/dist/antd.less'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(Antd);
 
 new Vue({
   render: h => h(App),
